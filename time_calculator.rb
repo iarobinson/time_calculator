db697f719bc2a49802cd16a248513042cb9e3c56
Ian Robinson <ian@ianrobinson.net>
2015-12-19 16:32:32 +1000

    Sketching out the idea
    Rough sketch of the tool

---
+++ time_calculator.rb
@@ -0,0 +1,20 @@
+# This will be the time calculator
+puts("Input your time stamped notes here with the time formatted like this [01:20] ")
+input = gets.chomp
+
+puts("Input the time you would like to add to the time stamps. Example [03:40] to add three minutes and 40 seconds.")
+add_time = gets.chomp
+
+idx = 0
+
+time_stamp = ""
+
+while idx < input.length
+  input.split("")
+  if input[0] == "[" || input[3] == ":" || input[6]
+    updated_time =
+    #Run Calculation
+    updated_time = add_time +
+  end
+  return new_time
+end
