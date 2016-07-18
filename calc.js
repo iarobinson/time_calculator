394ac93ba1796de47fe8be4dea9ccb1c926711ce
Ian Robinson <ian@ianrobinson.net>
2016-07-18 16:35:58 -0700

    Parsing Functions and Converting Time Strings to Second Integers
    Here I’ve skeletoned more of the complete calculation process and
    completed the function that converts a time string (like 1:30) to an
    integer of seconds (90).

--- calc.js
+++ calc.js
@@ -5,19 +5,38 @@
 // providing the ideas for what to do next
 
 
-function updateTimeStampNotes(existingNotes, incrementation) {
-  var updatedNotes = "";
-  for (var i = 0; i < existingNotes.length; i += 1) {
-    for (var j = i += 1; j < existingNotes.length; j += 1) {
-      if (existingNotes[i] === "[") {
-        var newTime = adjustTime(existingNotes[i]);
-        i += newTime.length;
-        // adjustTime needs to return a time block)
-      }
-    }
-  }
-  return updatedNotes;
+// function updateTimeStampNotes(existingNotes, incrementation) {
+//   var updatedNotes = "";
+//   for (var i = 0; i < existingNotes.length; i += 1) {
+//     if (existingNotes[i] === "[") {
+//       for (var j = i += 1; j < existingNotes.length; j += 1) {
+//         if (existingNotes[i] === "]") {
+//           var timeBlockToAdjust = existingNotes.slice(i + 1, j + 1);
+//           var updatedTime = timeCalculator(timeBlockToAdjust, incrementation);
+//         }
+//       }
+//     }
+//   }
+//   return updatedNotes;
+// }
+
+// Validated
+function formatTimeToInteger(str) {
+  var time = str.split(":")
+  console.log(time);
+  var min = parseInt(time[0]);
+  var sec = parseInt(time[1]);
+  var totalSeconds = (min * 60) + sec;
+  console.log(totalSeconds)
 }
 
+function intToStr(num) {
+    // This takes a integer of seconds and returns a string eg -> 100 => 1:40
+}
+
+function timeCalculator(timeBlockToAdjust, incrementation){
+  var incrementedTotalTime = formatTimeToInteger(timeBlockToAdjust) + formatTimeToInteger(incrementation);
+  return intToStr(incrementedTotalTime)
+}
 
 console.log(updateTimeStampNotes("<li>Note here [0:05]</li>", "0:20") + " <=> 0:25")
